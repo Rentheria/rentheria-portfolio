2516063436e5619534c0dbd653181d5f680b8ff4
Rentheria <rentheria.dev@gmail.com>
2025-09-08 19:43:57 -0600

    feature(project feature): Added card project Added animation Added Featured project

--- src/app/section/pages/overview/overview.component.ts
+++ src/app/section/pages/overview/overview.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../../shared/components/hero/hero.component';
 import { AboutComponent } from '../../../shared/components/about/about.component';
+import { ProjectComponent } from '../../../shared/components/project/project.component';
 
 @Component({
   selector: 'app-overview',
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, ProjectComponent],
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css'],
 })
---
+++ src/app/shared/components/card-featured-project/card-featured-project.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { FeatureProject } from '../../../../core/models/featureProject';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-featured-project',
+  imports: [CommonModule],
+  templateUrl: './card-featured-project.component.html',
+  styleUrl: './card-featured-project.component.css',
+})
+export class CardFeaturedProjectComponent {
+  @Input() project!: FeatureProject;
+}
